210255291d7238ed97923f7392e8bacaaa0f7fbd
Oluizn <antonioluiz.n.eng@gmail.com>
2024-08-07 07:14:48 -0300

    Programa finalizado

--- functions.c
+++ functions.c
@@ -22,7 +22,7 @@ void gerador (char *inicial, char *final) {
         else
             printf("Valor inicial é maior que final, por favor, insira um intervalo do menor para o maior.\n");
         fclose(file);
-        printf("Arquivo com sequencia hexadecimal gerada com sucesso\nO arquivo encontra-se pelo nome SerialNumber.txt");
+        printf("Arquivo com sequencia hexadecimal gerada com sucesso\nO arquivo encontra-se pelo nome SerialNumber.txt\n");
     }
     else
         printf("Falha na criação do arquivo.\n");
@@ -30,13 +30,11 @@ void gerador (char *inicial, char *final) {
 
 void menu() {
     char hexa_inicial[20], hexa_final[20];
-    printf("Seja bem vindo ao maior e melhor gerador de serial number em hexadecimal existente!!\n\n");
-    printf("Para gerar uma serie de numeros hexadecimal, insira o intervalo de numero que deseja gerar.\n");
-    printf("Exemplo: se deseja gerar uma serie de numeros hexadecimal a partir do valor 1A3F ate o valor 2A3F\nentao insira primeiro o menor valor, em seguida o maior.\n");
     printf("Insira o valor inicial (menor): \n");
     fgets(hexa_inicial, 20, stdin);
     printf("Insira o valor final (maior): \n");
     fgets(hexa_final, 20, stdin);
     gerador(hexa_inicial, hexa_final);
+    system("pause");
 }
 
